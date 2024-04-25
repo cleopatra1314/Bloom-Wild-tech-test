2f89e115d326f644c071e89fdbfb410315c5c40f
cleopatra <kelly912718@gmail.com>
2024-04-25 18:12:24 +0100

    Add rating star view.

--- B&W/SwiftUI/ProductDetailsSwiftUIView.swift
+++ B&W/SwiftUI/ProductDetailsSwiftUIView.swift
@@ -35,7 +35,11 @@ struct ProductDetailsSwiftUIView: View {
 //                    .frame(width: 300, height: 300)
                 
             Label(viewModel.price, systemImage: "dollarsign.circle")
-            Text(viewModel.rating.description)
+            HStack(spacing: 16.0) {
+                Text(viewModel.rating.description)
+                StarsView(rating: viewModel.rating, maxRating: 5)
+                    .frame(width: 100)
+            }
             Text(viewModel.description)
                 .multilineTextAlignment(.leading)
                 .lineLimit(0)
@@ -64,3 +68,31 @@ struct ProductDetailsSwiftUIView: View {
 //        ProductDetailsSwiftUIView(viewModel: )
 //    }
 //}
+
+struct StarsView: View {
+    var rating: CGFloat
+    var maxRating: Int
+
+    var body: some View {
+        let stars = HStack(spacing: 0) {
+            ForEach(0..<maxRating, id: \.self) { _ in
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+
+        stars.overlay(
+            GeometryReader { g in
+                let width = rating / CGFloat(maxRating) * g.size.width
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: width)
+                        .foregroundColor(.yellow)
+                }
+            }
+            .mask(stars)
+        )
+        .foregroundColor(.gray)
+    }
+}
